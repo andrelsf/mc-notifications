9819f0d57b487dbdae53a0484003bdb685a097b0
Andre Ferreira <andre.dev.linux@gmail.com>
2022-10-21 18:59:06 -0300

    Initial commit

---
+++ src/main/java/br/dev/multicode/mcnotifications/McNotificationsApplication.java
@@ -0,0 +1,13 @@
+package br.dev.multicode.mcnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McNotificationsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(McNotificationsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/dev/multicode/mcnotifications/McNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.multicode.mcnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McNotificationsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
